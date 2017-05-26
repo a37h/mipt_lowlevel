1cc5472b21ebda4a2a674af09a9c372eb958d822
a37h <justfastmeow@gmail.com>
2017-05-27 00:24:04 +0300

    Yes

--- Homeworks/Client_server_gol/Client/client.cpp
+++ Homeworks/Client_server_gol/Client/client.cpp
@@ -8,84 +8,61 @@
 #include <fcntl.h>             // O_CREAT O_RDWR (for shm_open)
     // Semaphores
 #include <semaphore.h>         // sem sem_wait sem_signal
-
-    // Some constants (should be the same both for server and client)
-#define STR_SIZE 1024
+    // Client and server same constants
 #include "../Constants.h"
-
-void Main_f(int area_x0, int area_y0, int area_x1, int area_y1);
-
+    // Client constants and global variables
+#define STR_SIZE 1024
 int* point = NULL;
 int* is_change = NULL;
 int* changed = NULL;
 char str[STR_SIZE];
-
 sem_t *sem;
 sem_t *sems[PROC_AMOUNT];
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-
 int socket_id;
+    // Some custom includes (uses global variables described earlier)
+#include "Libs/lib_NextGeneration.h"
+#include "Libs/lib_Main_f.h"
 
-#include "lib_NextGeneration.h"
-#include "lib_Main_f.h"
-
-int main(int argc,char **argv)
-{
-    //подключение к серверу
+int main(int argc,char **argv) {
+/*****************************************************************************************************/
+/************************************* Connection preparations ***************************************/
+    // Establishing a connection to the server
     struct protoent *protocol_record=NULL;
     struct hostent *host_record=NULL;
     struct sockaddr_in server_address;
-
     int sock_fd;
-
-    if(argc<2)
-    {
-        printf("Write the client server_ip\n");
-        return -1;
-    }
-
+    // Server ip is taken as first argument after ./client (otherwise, by default, it's "localhost")
+    char *connection_address;
+    if(argc<2) { connection_address =  (char*)"localhost"; }
+    else { connection_address = argv[1]; }
+    // Preparing tcp connection
     protocol_record=getprotobyname("tcp");
     socket_id=socket(PF_INET,SOCK_STREAM,protocol_record->p_proto);
+    host_record=gethostbyname(connection_address);
 
-    host_record=gethostbyname(argv[1]);
-    if(host_record==NULL)
-    {
-        printf("Can't get address by name '%s'\n",argv[1]);
-        return -1;
-    }
-
-
+/*****************************************************************************************************/
+/********************************** Establishing a connection ****************************************/
+    if(host_record==NULL) { printf("Can't get address by name '%s'\n",argv[1]); return -1; }
     server_address.sin_family=AF_INET;
     server_address.sin_port=htons(TCP_PORT_NUMBER);
     /*server_address.sin_addr.s_addr=inet_addr(argv[1]);*/
     memcpy(&server_address.sin_addr.s_addr, host_record->h_addr_list[0], host_record->h_length);
-
     printf("Connecting to server...\n");
-    sock_fd=
-            connect
-                    (
-                            socket_id,
-                            (struct sockaddr *)&server_address,
-                            sizeof(struct sockaddr_in)
-                    );
+    sock_fd=connect(socket_id, (struct sockaddr *)&server_address, sizeof(struct sockaddr_in));
+    if(sock_fd) { printf("Can't connect to server\n");return -1; }
 
-    if(sock_fd)
-    {
-        printf("Can't connect to server\n");
-        return -1;
-    }
-
+/*****************************************************************************************************/
+/***************************** Connection success, checking for problems *****************************/
     read(socket_id,str,18);
     str[16]='\0';
-    if(strcmp(str,"Server. Welcome."))
-    {
+    if(strcmp(str,"Server. Welcome.")) {
         printf("Incorrect server invatation\n");
         printf("Server tells '%s'\n",str);
-        return -1;
-    }
+        return -1; }
     printf("Server connected!\n");
-    //Подключение к серверу - конец
-
+/*****************************************************************************************************/
+/***************************************** Reading some inputs ***************************************/
     int area_x0, area_y0, area_x1, area_y1;
     read(socket_id, &area_x0, sizeof(int));
     read(socket_id, &area_y0, sizeof(int));
@@ -94,9 +71,6 @@ int main(int argc,char **argv)
     std::cout << area_x0 << ' ' << area_y0 << ' '
               <<  area_x1 << ' ' << area_y1 << std::endl;
     Main_f(area_x0, area_y0, area_x1, area_y1);
-
     close(socket_id);
-
     return 0;
-
 }
