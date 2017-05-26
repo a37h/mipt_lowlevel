7842c29adb7d15312b1b22dba9acd699826872c9
a37h <justfastmeow@gmail.com>
2017-05-26 06:38:56 +0300

    oh

--- Homeworks/Game_of_life/lib_parentfunc.h
+++ Homeworks/Game_of_life/lib_parentfunc.h
@@ -3,19 +3,54 @@
 
 #include <pthread.h>
 
+struct Args_box{
+    int* ptr1_c;
+    int* ptr2_c;
+    int* ptr3_c;
+    sem_t* sem1_c;
+    sem_t* sem2_c;
+    int shmid1_c;
+    int shmid2_c;
+    int shmid3_c;
+    pid_t *pid_c;
+};
+
 void Fork_function__parent(int* ptr1, int* ptr2, int* ptr3, sem_t* sem1, sem_t* sem2,
                            int shmid1, int shmid2, int shmid3, pid_t *pid)
 {
-    produce_thread(ptr1, ptr2, ptr3, sem1, sem2, shmid1, shmid2, shmid3, pid);
-}
+    struct Args_box test;
+    test.ptr1_c = ptr1;
+    test.ptr2_c = ptr2;
+    test.ptr3_c = ptr3;
+    test.sem1_c = sem1;
+    test.sem2_c = sem2;
+    test.shmid1_c = shmid1;
+    test.shmid2_c = shmid2;
+    test.shmid3_c = shmid3;
+    test.pid_c = pid;
 
+    produce_thread(&test);
+}
 
 void Child_processes_exit_checker(void *arg)
 {
+    struct Args_box * wrapped_args = (struct Args_box *) arg;
+
+    int *ptr1 = wrapped_args->ptr1_c;
+    int *ptr2 = wrapped_args->ptr2_c;
+    int *ptr3 = wrapped_args->ptr3_c;
+    sem_t *sem1 = wrapped_args->sem1_c;
+    sem_t *sem2 = wrapped_args->sem2_c;
+    int shmid1 = wrapped_args->shmid1_c;
+    int shmid2 = wrapped_args->shmid2_c;
+    int shmid3 = wrapped_args->shmid3_c;
+    pid_t pid = *(wrapped_args->pid_c);
+
     while (pid = waitpid (-1, NULL, 0)){
         if (errno == ECHILD)
             break;
     }
+
     /* shared memory detach */
     shmdt (ptr1);
     shmdt (ptr2);
@@ -30,81 +65,12 @@ void Child_processes_exit_checker(void *arg)
     exit (0);
 }
 
-
-
-void produce_thread(int* ptr1, int* ptr2, int* ptr3, sem_t* sem1, sem_t* sem2,
-                    int shmid1, int shmid2, int shmid3, pid_t *pid)
+void produce_thread(struct Args_box* wrapped_args)
 {
-    void* arguments_array[9];
-    arguments_array[0] = (void *) ptr1;
-    arguments_array[1] = (void *) ptr2;
-    arguments_array[2] = (void *) ptr3;
-    arguments_array[3] = (void *) sem1;
-    arguments_array[4] = (void *) sem2;
-    arguments_array[5] = (void *) &shmid1;
-    arguments_array[6] = (void *) &shmid2;
-    arguments_array[7] = (void *) &shmid3;
-    arguments_array[8] = (void *) pid;
+    void *argument_variable = (void *) wrapped_args;
 
     pthread_t tid;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    pthread_create(&tid, &attr, Child_processes_exit_checker, &arguments_array);
+    pthread_create(&tid, &attr, Child_processes_exit_checker, &argument_variable);
 }
-
-
-
-//
-//int not_main(int argc, char **argv)
-//{
-//    // array for pulling args to func
-//    int *argum[2];
-//    argum[0] = &sum;
-//    argum[1] = &limit;
-//
-//    // creating pthread (id (random), attr (random), constructor(attr), pthread creation)
-//    pthread_t tid; // it stores pthread id (which is unique)
-//    pthread_attr_t attr;
-//    pthread_attr_init(&attr);
-//    pthread_create(&tid, &attr, sum_runner, &argum);
-//
-//    int sum_temp = 0;
-//    // make that process part
-//    for (int i = 0; i < limit / 2; ++i) {
-//        sum_temp+=i;
-//    }
-//    adder(&sum, sum_temp);
-//
-//    // wait untill pthread is done it's calculating part
-//    pthread_join(tid, NULL);
-//
-//    // print result
-//    printf("result: %d\n", sum);
-//
-//    // destroying mutex lock
-//    pthread_mutex_destroy(&lock);
-//
-//    return 0;
-//}
-
-//pthread_mutex_t lock;
-// locked adding function
-//void* adder(int *sum, int value)
-//{
-//    pthread_mutex_lock(&lock);
-//    *sum += value;
-//    pthread_mutex_unlock(&lock);
-//    return NULL;
-//}
-// second process
-//void* sum_runner(void *arg)
-//{
-//    int **args = (int**) arg;
-//    int sum_temp = 0;
-//    for (int i = *args[1]/2; i <= *args[1]; ++i)
-//    {
-//        sum_temp += i;
-//    }
-//    adder(args[0], sum_temp);
-//    pthread_exit(0);
-//}
